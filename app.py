8d22d5d7354823e5bc01c1aa38a04a8f68f76d64
Kartavya Singh <kartavya.singh17@yahoo.com>
2023-08-17 00:36:47 -0400

    Email Feature Added

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import json
 import os, sys, time, datetime, random, string, re, requests
 from flask import Flask, Blueprint, render_template, request, session, redirect, url_for, jsonify, flash, current_app, g
 from flask_breadcrumbs import Breadcrumbs, register_breadcrumb
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 app = Flask(__name__)
 Breadcrumbs(app=app)
+secret_key = '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'
 
 # Define routes for each page
 @app.route('/')
@@ -33,10 +37,44 @@ def experiences():
 def pathways():
     return render_template('Pathways.html')
 
-@app.route('/Contact')
+@app.route('/Contact', methods=['GET', 'POST'])
 @register_breadcrumb(app, './Contact', 'Contact')
 def contact():
+    if request.method == 'POST':
+        # Process the form data here
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        if (not phone):
+            phone = "N/A"
+        location = request.form.get('location')
+        if (not location):
+            location = "N/A"
+        Subject = request.form.get('subject')
+        message = request.form.get('message')
+        
+        messageToSend = "Hi Kartavya, This is " + name + ". My email is " + email + ". My phone number is " + phone + ". I am from " + location + ". " + "\nI wanted to say:\n" + message + "."
+
+        # Send email to myself
+        message = Mail(
+            from_email='singhk6@mail.uc.edu',
+            to_emails='singhk6@mail.uc.edu',
+            subject=Subject,
+            html_content=f'<strong>{messageToSend}</strong>')
+        print(message)
+        try:
+            sg = SendGridAPIClient(secret_key)
+            response = sg.send(message)
+            print("Email sent successfully")
+            return "<script LANGUAGE='JavaScript'> window.alert('Message Successfully Sent!'); window.location.href = 'Contact'</script>)"
+        except Exception as e:
+            print("Error sending email")
+            print(e)
+            return "<script LANGUAGE='JavaScript'> window.alert('Message Failed To Send!'); window.location.href = 'Contact'</script>)"
+
+
     return render_template('Contact.html')
+
 
 if __name__ == "__main__":
     app.debug = True
